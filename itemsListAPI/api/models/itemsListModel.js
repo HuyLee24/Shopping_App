bca5f765ce5c38c6e4a57697553496896d366adb
HuyLee24 <mihawk240600@gmail.com>
2020-02-26 10:25:36 +0200

    a tested api

---
+++ itemsListAPI/api/models/itemsListModel.js
@@ -0,0 +1,24 @@
+//'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var ItemSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Kindly enter the name of the item'
+  },
+    Created_date: {
+        type: Date,
+        default: Date.now
+  },
+    status: {
+        type: [{
+            type: String,
+            enum: ['pending', 'ongoing', 'completed']
+    }],
+        default: ['pending']
+    }
+});
+
+module.exports = mongoose.model('Items', ItemSchema);
---
+++ itemsListAPI/server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
+    mongoose = require('mongoose'),
+    Item = require('./api/models/itemsListModel'), //created model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost:27017/ShoppingApp'); 
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    
+    
+    var routes = require('./api/routes/itemsListRoute'); //importing route
+    routes(app); //register the route
+
+// Middlewares
+    app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+    });
+
+app.listen(port);
+
+console.log('items list RESTful API server started on: ' + port);
